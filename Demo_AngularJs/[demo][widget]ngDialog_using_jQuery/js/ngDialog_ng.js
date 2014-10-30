96277f5695b7382f7322f518da6721017565dd2a
Qing, Lv <luqin@airbiquity.com>
2014-10-30 17:36:53 +0800

    add dialog demo

--- Demo_AngularJs/[demo][widget]ngDialog_using_jQuery/js/ngDialog_ng.js
+++ Demo_AngularJs/[demo][widget]ngDialog_using_jQuery/js/ngDialog_ng.js
@@ -1,5 +1,30 @@
 
-angular.module("ngDialog",[]).directive('myDialog', function() {
+angular.module("ngDialog",[])
+
+.directive('newDialog', function factory() {
+			return {
+						priority:100,
+						template: ['<div ng-show="visible">',
+											'    <h3>{{title}}</h3>',
+											'    <div class="body" ng-transclude></div>',
+											'    <div class="footer">',
+											'        <button ng-click="onOk()">OK</button>',
+											'        <button ng-click="onCancel()">Close</button>',
+											'    </div>',
+											'</div>'].join(""),
+						replace:false,
+						transclude: true,
+						restrict:'E',
+						scope:{
+									title:"@",//引用dialog标签title属性的值
+									onOk:"&",//以wrapper function形式引用dialog标签的on-ok属性的内容
+									onCancel:"&",//以wrapper function形式引用dialog标签的on-cancel属性的内容
+									visible:"@"//引用dialog标签visible属性的值
+						}
+			};
+})
+
+.directive('myDialog', function() {
 		return {
 				restrict: 'E',
 				transclude: true,
